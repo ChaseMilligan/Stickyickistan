444241c35fe6abf6b152062e8dd65f8b92010155
Chase Milligan <chasemilligan22@gmail.com>
2025-02-08 13:24:48 -0600

    Quartz sync: Feb 8, 2025, 1:24 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Wikiyickistan",
+    pageTitle: "🌐Wikiyickistan",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
